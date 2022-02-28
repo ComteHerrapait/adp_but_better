eff0debb4224d6f000b87db1095e23757338a821
Léon <l-delcroix@legallais.com>
2022-02-28 21:29:17 +0100

    new run script checking git status

--- run.sh
+++ run.sh
@@ -5,8 +5,21 @@ cd ${BASH_SOURCE%/*}
 
 echo "Program start."
 
-# install dependancies
-.venv/bin/pip3.10 install -r requirements.txt
+if [ "$(git rev-parse HEAD)" = "$(git rev-parse origin/master)" ]; then
+    echo "ADP But Better is up to date"
+else
+    echo "ADP But Better is NOT up to date with master"
+    read -p "Would you like to update using git ? (y/n)" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        # update local code
+        git pull origin/master
+        # install dependancies
+        .venv/bin/pip3.10 install -r requirements.txt
+    else
+        echo "running out of date version..."
+    fi
+fi
 
 # run main script
 .venv/bin/python3.10 main.py
