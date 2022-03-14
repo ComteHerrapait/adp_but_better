a4a6791415054bb12f710685727d26f4b6957253
ComteHerrapait <leon.delcroix@telecom-st-etienne.fr>
2022-03-14 19:48:50 +0100

    refactored user commands

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import json
 import logging
 import logging.config
-from datetime import datetime
 
 import inquirer
 from requests import Session
 
 from adp_wrapper.auth import SessionTimeoutException, adp_login
-from adp_wrapper.balance import get_balances
-from adp_wrapper.CLI_utils import print_header
+from adp_wrapper.CLI_utils import display_punch_times, print_header
+from adp_wrapper.user_commands import COMMAND_LIST, cmd_exit
 from adp_wrapper.constants import GOODBYE_MESSAGE, LOGGING_SETTINGS_FILE
 from adp_wrapper.punch import get_punch_times
-from adp_wrapper.user_commands import (
-    display_punch_times,
-    display_time_off_requests,
-    request_time_off,
-    search_users,
-    validate_and_punch,
-)
 
 
 def main_loop(session: Session):
@@ -25,15 +17,7 @@ def main_loop(session: Session):
         inquirer.List(
             "action",
             message="What do you want to do now ?",
-            choices=[
-                "Punch now",
-                "Punch at specific time",
-                "Get balance",
-                "Request time off",
-                "Search users",
-                "Get Timeoff Requests",
-                "Exit",
-            ],
+            choices=COMMAND_LIST.keys(),
             carousel=True,
         ),
     ]
@@ -43,61 +27,11 @@ def main_loop(session: Session):
     else:
         answer = "Exit"
 
-    match answer:
-        case "Punch now":
-            punch_time = datetime.now()
-
-            validate_and_punch(session, punch_time)
-            return True
-
-        case "Punch at specific time":
-            punch_time = datetime.now()
-            print("Specify punch time (values default to now)")
-            hour = int(input("Hour : ") or punch_time.hour)
-            minutes = int(input("Minutes : ") or punch_time.minute)
-            month = int(input("Month : ") or punch_time.month)
-            day = int(input("Day : ") or punch_time.day)
-            try:
-                punch_time = punch_time.replace(
-                    hour=hour, minute=minutes, month=month, day=day
-                )
-            except ValueError:
-                print("Cette horaire n'est pas valide")
-                return True
-
-            validate_and_punch(session, punch_time)
-            return True
-
-        case "Search users":
-            users = search_users(session)
-            if not users:
-                print("No users matching query found")
-            for user in users:
-                print(f"* {user['name']:<20} -> {user['id']}")
-            return True
-
-        case "Get balance":
-            balances = get_balances(session)
-            if not balances:
-                print("No balance found")
-
-            for balance in balances:
-                print(f"{balance['name']:<20}: {balance['value']} ({balance['unit']})")
-            return True
-
-        case "Request time off":
-            if request_time_off(session):
-                print("Time off request successfully sent")
-            else:
-                print("An error occured while sending the time off request")
-            return True
-
-        case "Get Timeoff Requests":
-            display_time_off_requests(session)
-            return True
-
-        case "Exit":
-            return False
+    user_command = COMMAND_LIST.get(answer)
+    if user_command is not None:
+        return user_command(session=session)
+    else:
+        return cmd_exit(session=session)
 
 
 def setup_logging():
@@ -142,6 +76,10 @@ if __name__ == "__main__":
             display_punch_times(timestamps)
         except KeyboardInterrupt:
             log.warning("application interrupted by user (ˆC)")
+            running = False
+        except AssertionError as ae:
+            log.critical(ae)
+            print(ae)
             running = False
 
     log.info("quitting app")
